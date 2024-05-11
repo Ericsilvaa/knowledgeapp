5f7e10e36264685c67672e8dfd48fb7f28585930
Eric <eriicsilva28@gmail.com.br>
2024-05-11 06:05:22 -0300

    refactor: re-organização de pastas

---
+++ app/dashboard/my-account/page.tsx
@@ -0,0 +1,7 @@
+export default function MyAccount() {
+  return (
+    <div>
+      <h1>MyAccount</h1>
+    </div>
+  )
+}
---
+++ app/dashboard/task-hub/page.tsx
@@ -0,0 +1,7 @@
+export default function TaskHubPage() {
+  return (
+    <div>
+      <h1>TaskHubPage aqui ficará no futuro um local para tasks</h1>
+    </div>
+  )
+}
